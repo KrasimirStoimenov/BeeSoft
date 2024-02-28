1bf86b921931e41687cd5846cd4b41db55c3553a
Krassimir Stoimenov <krassimir.stoimenov@gmail.com>
2024-02-28 15:45:25 +0200

    In progress work on home page

--- BeeSoft.Web/Controllers/HomeController.cs
+++ BeeSoft.Web/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 namespace BeeSoft.Web.Controllers;
 using System.Diagnostics;
 
+using BeeSoft.Services.Apiaries;
+using BeeSoft.Services.Hives;
 using BeeSoft.Web.Models;
+using BeeSoft.Web.Models.Apiaries;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Localization;
@@ -9,13 +12,27 @@ using Microsoft.AspNetCore.Mvc;
 
 using static BeeSoft.Common.GlobalConstants;
 
-public class HomeController() : Controller
+public class HomeController(IApiariesService apiariesService, IHivesService hivesService) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
         if (this.User.Identity.IsAuthenticated)
         {
-            return Redirect("Apiaries");
+            ICollection<ApiaryHivesViewModel> apiariesWithHivesViewModel = new List<ApiaryHivesViewModel>();
+
+            var apiaries = await apiariesService.GetApiariesAsync();
+            foreach (var apiary in apiaries)
+            {
+                var hives = await hivesService.GetHivesInApiaryAsync(apiary.Id);
+
+                apiariesWithHivesViewModel.Add(new ApiaryHivesViewModel
+                {
+                    ApiaryName = apiary.Name,
+                    Hives = hives,
+                });
+            }
+
+            return View(apiariesWithHivesViewModel);
         }
 
         return Redirect("Identity/Account/Login");
