36461e4f52508adaecf791dc23db706399ce09b5
Krassimir Stoimenov <krassimir.stoimenov@gmail.com>
2024-04-25 15:48:14 +0300

    Added modal for delete functionality

--- BeeSoft.Web/wwwroot/js/site.js
+++ BeeSoft.Web/wwwroot/js/site.js
@@ -14,9 +14,37 @@ function changeCulture() {
     }
 
     $.ajax({
-        type: 'GET',
+        method: 'GET',
         url: '/Home/SetLanguage',
         data: { culture: culture },
+        success: function () {
+            location.reload();
+        },
+        error: function (error) {
+            alert(error.responseText);
+        }
+    });
+}
+
+function showDeleteModal(id) {
+    $('#deleteModal').modal('show')
+    $('#deleteBtn').attr('item-id',id);
+}
+
+function deleteItem() {
+    const deleteBtn = document.getElementById('deleteBtn');
+    const itemIdAttr = deleteBtn.getAttribute('item-id');
+    const itemId = Number(itemIdAttr);
+
+    const pathElements = deleteBtn.ownerDocument.location.pathname
+        .split('/')
+        .filter(x => x);
+
+    const controllerName = pathElements.length > 1 ? pathElements[1] : pathElements[0];
+
+    $.ajax({
+        url: `/${controllerName}/Delete`,
+        data: { id: itemId },
         success: function () {
             location.reload();
         },
