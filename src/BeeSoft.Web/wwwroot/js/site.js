62673abf5127281d8a0a40ccd1132e8b3b18e8e8
Krassimir Stoimenov <krassimir.stoimenov@gmail.com>
2024-05-11 17:03:59 +0300

    extract delete item js functions to separate js file instead site.js

--- src/BeeSoft.Web/wwwroot/js/site.js
+++ src/BeeSoft.Web/wwwroot/js/site.js
@@ -25,31 +25,3 @@ function changeCulture() {
         }
     });
 }
-
-function showDeleteModal(id) {
-    $('#deleteModal').modal('show')
-    $('#deleteBtn').attr('item-id',id);
-}
-
-function deleteItem() {
-    const deleteBtn = document.getElementById('deleteBtn');
-    const itemIdAttr = deleteBtn.getAttribute('item-id');
-    const itemId = Number(itemIdAttr);
-
-    const pathElements = deleteBtn.ownerDocument.location.pathname
-        .split('/')
-        .filter(x => x);
-
-    const controllerName = pathElements.length > 1 ? pathElements[1] : pathElements[0];
-
-    $.ajax({
-        url: `/${controllerName}/Delete`,
-        data: { id: itemId },
-        success: function () {
-            location.reload();
-        },
-        error: function (error) {
-            alert(error.responseText);
-        }
-    });
-}
