90120aca57615fc071d2ae9e2dba743f1c3c9606
Krassimir Stoimenov <krassimir.stoimenov@gmail.com>
2024-05-11 22:15:51 +0300

    delete item js ajax call changed to use JS instead jQuery

--- src/BeeSoft.Web/wwwroot/js/deleteItem.js
+++ src/BeeSoft.Web/wwwroot/js/deleteItem.js
@@ -3,7 +3,7 @@
     $('#deleteBtn').attr('item-id', id);
 }
 
-function deleteItem() {
+async function deleteItem() {
     const deleteBtn = document.getElementById('deleteBtn');
     const itemIdAttr = deleteBtn.getAttribute('item-id');
     const itemId = Number(itemIdAttr);
@@ -14,14 +14,27 @@ function deleteItem() {
 
     const controllerName = pathElements.length > 1 ? pathElements[1] : pathElements[0];
 
-    $.ajax({
-        url: `/${controllerName}/Delete`,
-        data: { id: itemId },
-        success: function () {
-            location.reload();
-        },
-        error: function (error) {
-            alert(error.responseText);
+    try {
+        const res = await fetch(`/${controllerName}/Delete/`+ itemId);
+
+        if (res.status != 200) {
+            throw new Error(`${res.status}:${res.statusText}`);
         }
-    });
+
+        location.reload();
+
+    } catch (error) {
+        alert(error.responseText);
+    }
+
+    //$.ajax({
+    //    url: `/${controllerName}/Delete`,
+    //    data: { id: itemId },
+    //    success: function () {
+    //        location.reload();
+    //    },
+    //    error: function (error) {
+    //        alert(error.responseText);
+    //    }
+    //});
 }
